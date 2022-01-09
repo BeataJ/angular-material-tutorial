6d872fa31f57c64a4e239b666d7f4b1153d029ea
beataj <beata@emc2wave.com>
2022-01-09 00:18:13 -0800

    add material angular syntax to app component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { MaterialModule } from './material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
